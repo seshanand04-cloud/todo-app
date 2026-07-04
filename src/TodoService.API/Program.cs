8ef223a9d194f834c02ffbfa869461238d0bb423
Seshanand <seshanand04@gmail.com>
2026-07-04 10:03:20 +0530

    Trigger IIS deploy test

--- src/TodoService.API/Program.cs
+++ src/TodoService.API/Program.cs
@@ -16,7 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "chilly start", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "chilly ", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
